71ef81dcbbb546e11cf9bab1092c75306d98769b
Crowdbotics <engineering@crowdbotics.com>
2023-08-22 15:24:25 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "gabesapp-2",
+  url: "https://gabesapp-2.botics.co",
+  api: "https://gabesapp-2.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
